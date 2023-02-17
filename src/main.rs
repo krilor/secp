da41efb8213116ffe59cabce0b3eb22a18d98a52
Kristoffer Lorentsen <krilor@gmail.com>
2023-02-17 20:34:05 +0100

    log

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use anyhow::{Context, Result};
+use log::{info,warn};
 
 #[derive(Parser)]
 /// Copy files to a remote machine with sudo on the other end
@@ -13,7 +14,10 @@ struct Secp {
 }
 
 fn main() -> Result<()> {
+    env_logger::init();
     let args = Secp::parse();
+
+    info!("got file {}",&args.source);
 
     let content =  std::fs::read_to_string(&args.source)
         .with_context(|| format!("not able to read file `{}`", &args.source))?;
